5c5135086e291eb38adce6cb16d13a50890b162b
Jadsonvs <jadson.vsantos2013@gmail.com>
2023-05-27 16:09:12 -0300

    Adicionado dependência no Pom e criado a classe EmailService

---
+++ src/main/kotlin/br/com/alura/forum/service/EmailService.kt
@@ -0,0 +1,24 @@
+package br.com.alura.forum.service
+
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+
+@Service
+class EmailService(
+    private val javaMailSender: JavaMailSender
+) {
+
+    fun notification() {
+        val message = SimpleMailMessage()
+
+        message.apply {
+            subject = ""
+            text = ""
+            setTo("")
+        }
+
+
+    }
+
+}
